3b0a8f4bfc0bcb5fcdb49c244b557d1d61f98d9b
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-02-10 14:41:30 -0500

    Update Copyright text

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Authors';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : '2023 © All rights reserved.';
+    : '© 2023 authors.mytcreative.com. All rights reserved.';
 
   return {
     name,
