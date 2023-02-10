a79a2412eeeccb50e93f15d00648a205b9dc70ce
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-02-10 14:38:52 -0500

    Remove gradient from logo

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,8 @@ import Image from 'next/image';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src='https://mytcreative.com/IMG/logo_clear.svg' className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      {/* <img src='https://mytcreative.com/IMG/logo_clear.svg' className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
+      <img src='https://mytcreative.com/IMG/logo_clear.svg' className="w-12 h-12 rounded-full block mx-auto mb-4" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
