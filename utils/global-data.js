415979a12d1ea8dfb694444d6352c4b4771796ac
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-02-09 21:44:22 -0500

    Update Globals

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Authors';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '&copy; 2023 authors.mytCreative.com. All rights reserved.';
 
   return {
     name,
